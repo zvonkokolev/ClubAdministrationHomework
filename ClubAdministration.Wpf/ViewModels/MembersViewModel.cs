5bd7ca9ccb66c13d8709c18ab873050187820004
Name <Name@DESKTOP-6ADNBQH>
2020-11-14 17:57:28 +0100

    Ver. 0.5 members window

--- ClubAdministration.Wpf/ViewModels/MembersViewModel.cs
+++ ClubAdministration.Wpf/ViewModels/MembersViewModel.cs
@@ -1,17 +1,28 @@
-﻿using ClubAdministration.Core.Entities;
+﻿using ClubAdministration.Core.Contracts;
+using ClubAdministration.Core.Entities;
+using ClubAdministration.Persistence;
 using ClubAdministration.Wpf.Common;
 using ClubAdministration.Wpf.Common.Contracts;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
+using System.Windows.Input;
 
 namespace ClubAdministration.Wpf.ViewModels
 {
     public class MembersViewModel : BaseViewModel
     {
+        private ICommand _cmdSaveMember;
+        private Member _member;
+        private string _lastName;
+        private string _firstName;
+
         public MembersViewModel(IWindowController windowController, Member member) : base(windowController)
         {
+            Member = member;
+            LastName = member.LastName;
+            FirstName = member.FirstName;
             LoadCommands();
         }
 
@@ -23,6 +34,72 @@ namespace ClubAdministration.Wpf.ViewModels
         public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
             throw new NotImplementedException();
+        }
+
+        // commands
+        public ICommand CmdSaveMember 
+        {
+            get
+            {
+                if(_cmdSaveMember == null)
+                {
+                    _cmdSaveMember = new RelayCommand(
+                        execute: _ =>
+                        {
+                            IUnitOfWork uow = new UnitOfWork();
+                            try
+                            {
+                                uow.SaveChangesAsync();
+                            }
+                            catch (Exception)
+                            {
+                                throw;
+                            }
+                        }
+                    , canExecute: _ => Member != null
+                    );
+                }
+                return _cmdSaveMember;
+            }
+            set
+            {
+                _cmdSaveMember = value;
+            }
+        }
+
+        public Member Member
+        {
+            get => _member;
+            private set
+            {
+                _member = value;
+                OnPropertyChanged(nameof(Member));
+                Validate();
+            }
+        }
+        [Required]
+        [MinLength(2, ErrorMessage = "Lastname must be at least 2 character long")]
+        public string LastName 
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged(nameof(LastName));
+                //Validate();
+            } 
+        }
+        [Required]
+        [MinLength(2, ErrorMessage = "Firstname must be at least 2 character long")]
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged(nameof(FirstName));
+                //Validate();
+            } 
         }
     }
 }
